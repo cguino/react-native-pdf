95b6bd920c64610fa4ff990c4061bc4c4fe2a2bc
Antoine Richeux <a.richeux@beapp.fr>
2020-06-24 14:59:41 +0200

    hide scroll view indicator

--- ios/RCTPdf/RCTPdfView.m
+++ ios/RCTPdf/RCTPdfView.m
@@ -263,7 +263,7 @@ const float MIN_SCALE = 1.0f;
             }
         }
         
-        
+        [[[[_pdfView subviews] firstObject] scrollView] setShowsHorizontalScrollIndicator: NO];
         [_pdfView layoutDocumentView];
         [self setNeedsDisplay];
     }
